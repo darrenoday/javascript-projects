bf6845cc29fd7f8239da143284b54968b8859297
Darren Oday <darren.oday@gmail.com>
2024-02-10 21:57:37 -0600

    finished exercise

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,10 +1,20 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
-  }
+    try{let result = lab.runLab(3)
+    console.log(`${lab.student} code worked: ${result === 27}`);}
+    catch(typeError){
+      console.log(result = "Error thrown")
+    }
+  }     
+  
 }
+   
+    
+
+
+
+
 
 let studentLabs = [
   {
@@ -21,4 +31,27 @@ let studentLabs = [
   }
 ];
 
-gradeLabs(studentLabs);
+//gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
